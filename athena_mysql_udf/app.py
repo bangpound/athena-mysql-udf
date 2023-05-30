ebb4513c87f3686e4b7b403a7a081840d1cd7f9a
Benjamin Doherty <bdoherty@activecampaign.com>
2023-05-30 10:24:24 -0400

    Memoize results of distill so that we don't call it more than we need to.

--- athena_mysql_udf/app.py
+++ athena_mysql_udf/app.py
@@ -1,12 +1,20 @@
 import athena_udf
 import mysql_distill
+from functools import cache
+
+
+@cache()
+def cached_distill(digest_text: str):
+    if digest_text:
+        return mysql_distill.distill(digest_text)
+    return ""
 
 
 class MysqlUDFHandler(athena_udf.BaseAthenaUDF):
     @staticmethod
     def handle_athena_record(input_schema, output_schema, arguments):
         digest_text = arguments[0]
-        return mysql_distill.distill(digest_text)
+        return cached_distill(digest_text)
 
 
 lambda_handler = MysqlUDFHandler().lambda_handler
